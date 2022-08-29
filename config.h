c544f7b47be61d49e04dd1c13ae31892b0edb813
Jon Arne Viksås <hackjack@tutanota.com>
2022-08-29 14:21:50 +0200

    Fixed over senibility on Home row tap hold. This makes it betther on key rolover. Fast "ka" does not trigger ctrl+a.

--- config.h
+++ config.h
@@ -6,3 +6,17 @@
 #define LEADER_PER_KEY_TIMING
 #define LEADER_TIMEOUT 250
 
+
+
+// --------------------------------------------------
+// Homerow modifier setup. This prevent some rolover effect on Tap hold.
+// https://precondition.github.io/home-row-mods#getting-started-with-home-row-mods-on-qmk
+#define TAPPING_TERM 200
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+// --------------------------------------------------
+#
